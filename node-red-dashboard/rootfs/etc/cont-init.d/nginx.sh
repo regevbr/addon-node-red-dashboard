f6e3440be1b4e4f82f7a69e231650c2de6b48fe5
regevbr <regevbr@gmail.com>
2021-10-07 15:04:16 +0300

    rebrand

--- node-red-dashboard/rootfs/etc/cont-init.d/nginx.sh
+++ node-red-dashboard/rootfs/etc/cont-init.d/nginx.sh
@@ -14,3 +14,6 @@ ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+
+dns_host=$(bashio::dns.host)
+sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/includes/resolver.conf
