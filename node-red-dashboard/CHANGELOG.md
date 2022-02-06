9c9340a859063877455410dbe0992013ee77e1a3
Regev Brody <regevbr@gmail.com>
2022-02-06 08:19:20 +0200

    Docker (#11)
    * fix ups
    
    * fix ups
    
    * fix ups
    
    * fix ups

--- node-red-dashboard/CHANGELOG.md
+++ node-red-dashboard/CHANGELOG.md
@@ -1,5 +1,14 @@
 # Changelog
 
+## [1.2.0] - 2022-02-06
+
+### Changed
+
+- Moved to yaml configuration
+- Moved to templating technique from sed to tempio
+- Changelog to contain links
+- Removed unused docker dependencies
+
 ## [1.1.1] - 2022-02-05
 
 ### Fixed
@@ -25,8 +34,14 @@
 
 - Fixed changelog
 
-## [1.0.0] - 2022-02-05
+## 1.0.0 - 2022-02-05
 
 ### Added
 
 - Official stable release
+
+[1.0.1]: https://github.com/regevbr/addon-node-red-dashboard/compare/v1.0.0...v1.0.1
+[1.0.2]: https://github.com/regevbr/addon-node-red-dashboard/compare/v1.0.1...v1.0.2
+[1.1.0]: https://github.com/regevbr/addon-node-red-dashboard/compare/v1.0.2...v1.1.0
+[1.1.1]: https://github.com/regevbr/addon-node-red-dashboard/compare/v1.1.0...v1.1.1
+[1.2.0]: https://github.com/regevbr/addon-node-red-dashboard/compare/v1.1.1...v1.2.0
--- node-red-dashboard/rootfs/etc/cont-init.d/nginx.sh
+++ node-red-dashboard/rootfs/etc/cont-init.d/nginx.sh
@@ -3,14 +3,16 @@
 # Home Assistant Community Add-on: Node-RED dashboard
 # Configures NGINX for use with Node-RED dashboard
 # ==============================================================================
-declare dns_host
-declare ingress_interface
-declare ingress_port
 
-ingress_port=$(bashio::addon.ingress_port)
-ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+bashio::var.json \
+    interface "$(bashio::addon.ip_address)" \
+    port "^$(bashio::addon.ingress_port)" \
+    | tempio \
+        -template /etc/nginx/templates/ingress.gtpl \
+        -out /etc/nginx/servers/ingress.conf
 
-dns_host=$(bashio::dns.host)
-sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/includes/resolver.conf
+bashio::var.json \
+    dns_host "$(bashio::dns.host)" \
+    | tempio \
+        -template /etc/nginx/templates/resolver.gtpl \
+        -out /etc/nginx/includes/resolver.conf
